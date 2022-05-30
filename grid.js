246e816bab45f761a0b46cedfd428e8a54366319
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-30 11:44:52 -0500

    Made the grid 2 dimensional

--- grid.js
+++ grid.js
@@ -3,11 +3,14 @@ function createGrid() {
   //makes a 10 x 10 grid
   //this will make a button
   //at location 30,30
-  g.push(new Clickable(30, 30));
-  for (i = 0; i < 5; i++) {
-    g[i].resize(30, 30);
-    g[i].text = "";
-    g[i].cornerRadius = 0;
-    g[i].color = "purple";
-  } //end loop
+  for (j = 0; j < 10; j++) {
+    g[j] = [];
+    for (i = 0; i < 10; i++) {
+      g[j][i] = new Clickable(30 + j * 30, 30 + i*30);
+      g[j][i].resize(30, 30);
+      g[j][i].text = "";
+      g[j][i].cornerRadius = 0;
+      g[j][i].color = "purple";
+    } //end i loop
+  } //end j loop
 }
