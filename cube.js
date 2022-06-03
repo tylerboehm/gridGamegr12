f2fb926bfa2f2cad5db2c172241359706a5e3384
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-03 11:13:17 -0500

    The Cube should change the Grid

--- cube.js
+++ cube.js
@@ -12,9 +12,14 @@ function createCube(){
     c.isPressed = false;
     c.x = Math.floor(c.x / 30)*30;
     c.y = Math.floor(c.y / 30)*30;
+    placeCube();
   }
 }//end createCube
 
 function placeCube(){
+  let thisX = c.x / 30 - 1;
+  let thisY = c.y / 30 - 1;
+  g[thisX][thisY].color = c.color;
+  c.x = 600
   
 }
