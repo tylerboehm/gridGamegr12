e5a49b1e8be3eed7bf6de2746c62c64e8f03609e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-03 11:32:53 -0500

    Update el.js
    Added the horizontal piece

--- el.js
+++ el.js
@@ -1,26 +1,34 @@
-let el;;
+let el;
 let el2;
 
-function createEl(){
-  el = new Clickable(100,360);
-  el.resize(30,60);
+function createEl() {
+  el = new Clickable(100, 360);
+  el2 = new Clickable(100, 390);
+  el.resize(30, 60);
+  el2.resize(60, 30);
   el.color = "pink";
+  el2.color = "pink";
   el.text = "";
-  el.onPress = function(){
-    el.isPressed = true;    
-  }
-  el.onRelease = function(){
+  el2.text = "";
+  el.onPress = function () {
+    el.isPressed = true;
+    el2.isPressed = true;
+  };
+  el.onRelease = function () {
     el.isPressed = false;
-    el.x = Math.floor(el.x / 30)*30;
-    el.y = Math.floor(el.y / 30)*30;
+    el2.isPressed = false;
+    el.x = Math.floor(el.x / 30) * 30;
+    el.y = Math.floor(el.y / 30) * 30;
+    el2.x =el.x
+    el2.y = el.y + 30
     placeEl();
-  }
-}//end createCube
+  };
+} //end createCube
 
-function placeEl(){
+function placeEl() {
   let thisX = el.x / 30 - 1;
   let thisY = el.y / 30 - 1;
   g[thisX][thisY].color = el.color;
-  el.x = 600
-  
+  el.x = 600;
+  el2.x = 600
 }
