15b88ce7524d8e87fdcc0cccc35ae2c258b3b1a6
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-06 11:28:25 -0500

    Update cube.js

--- cube.js
+++ cube.js
@@ -1,25 +1,36 @@
-let c;
+let el;
+let el2;
 
-function createCube(){
-  c = new Clickable(1000,360);
-  c.resize(60,60);
-  c.color = "green";
-  c.text = "";
-  c.onPress = function(){
-    c.isPressed = true;    
-  }
-  c.onRelease = function(){
-    c.isPressed = false;
-    c.x = Math.floor(c.x / 30)*30;
-    c.y = Math.floor(c.y / 30)*30;
-    placeCube();
-  }
-}//end createCube
+function createEl() {
+  el = new Clickable(1000, 360);
+  el2 = new Clickable(1000, 390);
+  el.resize(30, 60);
+  el2.resize(60, 30);
+  el2.strokeWeight = 0;
+  el.strokeWeight = 0;
+  el.color = "pink";
+  el2.color = "pink";
+  el.text = "";
+  el2.text = "";
+  el.onPress = function () {
+    el.isPressed = true;
+    el2.isPressed = true;
+  };
+  el.onRelease = function () {
+    el.isPressed = false;
+    el2.isPressed = false;
+    el.x = Math.floor(el.x / 30) * 30;
+    el.y = Math.floor(el.y / 30) * 30;
+    el2.x =el.x
+    el2.y = el.y + 30
+    placeEl();
+  };
+} //end createCube
 
-function placeCube(){
-  let thisX = c.x / 30 - 1;
-  let thisY = c.y / 30 - 1;
-  g[thisX][thisY].color = c.color;
-  c.x = 600
-  
+function placeEl() {
+  let thisX = el.x / 30 - 1;
+  let thisY = el.y / 30 - 1;
+  g[thisX][thisY].color = el.color;
+  el.x = 600;
+  el2.x = 600
 }
