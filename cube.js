d5d25cc921bb5630044189283b7736bb5816e597
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-09 11:16:27 -0500

    Now Checks the boundaries

--- cube.js
+++ cube.js
@@ -10,14 +10,34 @@ function createCube(){
   }
   c.onRelease = function(){
     c.isPressed = false;
-    checkInGrid(c);
+    checkCube();
     c.x = Math.floor(c.x / 30)*30;
     c.y = Math.floor(c.y / 30)*30;
     placeCube();
     checkForThree();
   }
 }//end createCube
-
+function checkCube(){
+  if(c.x < 30){
+    //the shape off the grid to the left.
+    c.x = 30;
+  }
+  if(c.y < 30){
+    //the shape is off the top of 
+    //the grid.
+    c.y = 30;
+  }
+  if(c.y > 330){
+    //the shape is off the top of 
+    //the grid.
+    c.y = 270;
+  }
+   if(c.x > 330){
+    //the shape is off the top of 
+    //the grid.
+    c.x = 270;
+  }
+}
 function placeCube(){
   let thisX = c.x / 30 - 1;
   let thisY = c.y / 30 - 1;
